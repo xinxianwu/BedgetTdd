7bd4128e0a4b9204f64e778666485404d175e035
xinxian <sgmp2555029@gmail.com>
2023-11-25 20:16:27 +0800

    test: cleanup test case

--- BudgetTdd/UnitTest1.cs
+++ BudgetTdd/UnitTest1.cs
@@ -8,22 +8,35 @@ namespace BudgetTdd;
 
 public class Tests
 {
+    private BudgetService _budgetService;
+    private IBudgetRepo _budgetRepo;
+
     [SetUp]
-    public void Setup()
+    public void SetUp()
     {
+        _budgetRepo = Substitute.For<IBudgetRepo>();
+        _budgetService = new BudgetService(_budgetRepo);
     }
 
     [Test]
     public void QueryMonthly()
     {
-        var budgetRepo = Substitute.For<IBudgetRepo>();
-        budgetRepo.GetAll().Returns(new List<BudgetDateModel>
+        GivenBudgets(new List<BudgetDateModel>
         {
             new() { Amount = 30 }
         });
-        var budgetService = new BudgetService(budgetRepo);
-        var budget = budgetService.Query(new DateTime(2023, 11, 01), new DateTime(2023, 11, 30));
+        var budget = _budgetService.Query(new DateTime(2023, 11, 01), new DateTime(2023, 11, 30));
 
-        budget.Should().Be(30);
+        BudgetShouldBe(budget, 30);
+    }
+
+    private static void BudgetShouldBe(decimal budget, int expected)
+    {
+        budget.Should().Be(expected);
+    }
+
+    private void GivenBudgets(List<BudgetDateModel> budgetDateModels)
+    {
+        _budgetRepo.GetAll().Returns(budgetDateModels);
     }
 }
